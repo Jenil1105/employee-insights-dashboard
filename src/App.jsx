7b6a24c47d26273c35e29f4315129bb2ad1cf8fb
Jenil1105 <unselfstudy@gmail.com>
2026-03-15 11:56:14 +0530

    SetUp Basic App

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <h1>Let's get started...</h1>
+    </>
+  )
+}
+
+export default App
